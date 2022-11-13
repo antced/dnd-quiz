bac3b938691c0ee0eb69e30948e33f1143389474
Anthony Cedrone <anthonycedrone@Anthonys-Air.fios-router.home>
2022-11-13 13:42:42 -0500

    made start button change question and build answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
+var quiz = [{
+    question: "This is a test question",
+    answers: {
+        1: "wrong",
+        2: "wrong",
+        3: "right",
+        4: "wrong"
+    },
+    rightAnswer: "right",
+}, {
+    question: "This is another test question",
+    answers: {
+        1: "right",
+        2: "wrong",
+        3: "wrong",
+        4: "wrong"
+    },
+    rightAnswer: "right",
+}
+]
+
+var questionNumber = 0
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
-document.getElementById("startBtn").addEventListener("click",createButton);
+document.getElementById("startBtn").addEventListener("click", makeQuiz);
 // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
 // THEN I am presented with another question
@@ -11,7 +34,16 @@ document.getElementById("startBtn").addEventListener("click",createButton);
 // WHEN the game is over
 // THEN I can save my initials and my score
 
-function createButton() {
+function makeQuiz() {
+
+    var startBtn = document.getElementById("startBtn");
+    startBtn.remove();
+
+// rename the h1 to question name
+    var question = document.getElementById("questionText");
+    question.textContent = quiz[questionNumber].question;
+
+// create 4 buttons
     var newBtn = document.createElement("button");
     document.getElementById("buttonDiv").appendChild(newBtn);
     var newBtn = document.createElement("button");
@@ -20,9 +52,16 @@ function createButton() {
     document.getElementById("buttonDiv").appendChild(newBtn);
     var newBtn = document.createElement("button");
     document.getElementById("buttonDiv").appendChild(newBtn);
-    
+
+// maybe replace with get all?
     var buttonDiv = document.getElementById("buttonDiv");
     var buttonArr = buttonDiv.getElementsByTagName("button");
-    console.log(buttonArr[0]);
-    
+
+    // apply the answer options to the button text
+    buttonArr[questionNumber].textContent = "blank"
+    buttonArr[questionNumber + 1].textContent = "gfdfgh"
+    buttonArr[questionNumber + 2].textContent = "gfdfgh"
+    buttonArr[questionNumber + 3].textContent = "gfdfgh"
+
+    questionNumber++
 }
